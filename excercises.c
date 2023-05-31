786cb5c68481e46b45713d5866d6e2168e085b0e
boouke <119793653+boouke@users.noreply.github.com>
2023-05-31 11:55:23 +0200

    Update excercises.c

--- excercises.c
+++ excercises.c
@@ -1,85 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <sys/stat.h>
 
-// Function prototypes
-int for_loop_excercise();
-int num_c(int *max, char *err01, char *err02, char *buffer);
-int even_or_odd_c(char *even_or_odd, char *buffer, char *err03);
-int math(char *even_or_odd, int max, int sum, int i);
 
-// Function initiating all other programs
+#define WELCOME_MSG "\nWelcome to the BLK File Management System!\n"
+#define CHOOSE_MSG "Choose if you want to create, delete or rename a file! You can also now create directories using the directory input!\n"
+#define CREATE_MSG "You have selected creation mode! Type in the file name (.txt) and adress starting at the root folder that you want to create!\n"
+#define DIR_CREATE_MSG "You have selected directory creation mode! Type in the dir's adress with it's name starting at the root folder.\n"
+#define DELETE_MSG "You have selected deleting mode! Type in the files current adress starting at the root folder and it will be deleted!\n"
+#define SUCCESS_MSG "Operation succesful!\n"
+#define RENAME_MSG "You have selected renaming mode! Type in the files current adress starting at the root folder and name and a new name for it!\n"
+#define ERR_LEN "UNKNOWN ERROR(0x1) Please contact our support team ar github.com∕boouke and provide the memory adress"
+#define ERR_FILE_CREATE "FILE CREATE ERROR(0x2) Please restart the program, and try again. If you encounter the same problem contact our support team at github.com/boouke\n"
+
+void choose(int* int_operation);
+void create(void);
+void name(void);
+void dir_create(void);
+void file_delete(void);
+
 int main(){
-    printf("Starting program...\n");
-    for_loop_excercise();
+    int int_operation = 0;
+    printf("%s", WELCOME_MSG);
+    choose(&int_operation);
+    if(int_operation == 1){
+        create();
+        return 0;
+    }
+    else if(int_operation == 2){
+        file_delete();
+        return 0;
+    }
+    else if(int_operation == 3){
+        dir_create();
+        return 0;
+    }
+    else if(int_operation == 4){
+        name();
+        return 0;
+    }
     return 0;
+}
+void choose(int* int_operation){
+    int len = 50;
+    char operation[200];
+    char* buffer = (char*)malloc((len + 1) * sizeof(char));
+    printf("%s", CHOOSE_MSG);
+    while (scanf("%s", operation) != 0 && (strcmp(operation, "create") != 0 && strcmp(operation, "delete") != 0 && strcmp(operation, "directory") != 0) && strcmp(operation, "rename") != 0){
+        buffer = (char*)realloc(buffer, (strlen(operation) + 1) * sizeof(char));
+        printf("%s is not a valid option. Please enter 'create', 'delete', 'rename' or 'directory':\n", operation);
+    }
+    if(strcmp(operation, "create") == 0){
+        *int_operation = 1;
+        return;
+    }
+    else if(strcmp(operation, "delete") == 0){
+        *int_operation = 2;
+        return;
+    }
+    else if(strcmp(operation, "directory") == 0){
+        *int_operation = 3;
+        return;
+    }
+    else if(strcmp(operation, "rename") == 0){
+        *int_operation = 4;
+        return;
+    }
+    free(buffer);
+}
+void create(void) {
+    int len = 50;
+    char* file_name = (char*)malloc((len + 1) * sizeof(char));
+    printf("%s", CREATE_MSG);
+    while (scanf("%s", file_name) != 1){
+        printf("%s at %p", ERR_LEN, file_name);
+    }
+    FILE* file = fopen(file_name, "w");
+    if (file == NULL){
+        printf("%s", ERR_FILE_CREATE);
+    } else{
+        printf("%s", SUCCESS_MSG);
+        fclose(file);
+    }
+    free(file_name);
 }
 
-//The for loop excercise
-int for_loop_excercise(){   
-    int i = 1; //looping variable
-    int sum = 0; //sum of integers in the math func
-    char even_or_odd[4]; //type of result in math func
-    int max; //max integer for math result
-    char buffer[100]; //input buffer
-    // error messages
-    char err01[100] = "ERROR 01 - INPUT IS STRING. PLEASE INSERT A VALID NUMBER.\n";
-    char err02[100] = "ERROR 02 - INVALID INTEGER. PLEASE INSERT A VALID NUMBER.\n";
-    char err03[100] = "ERROR 03 - INVALID INPUT. PLEASE INSERT EITHER even OR odd.\n";
-    //main part
-    printf("Welcome to the program that gives you all of the integers below the one that you give, even or odd! Insert an integer.\n");
-    num_c(&max, err01, err02, buffer);
-    printf("You have selected %d!\nNow select if you want odd, or even numbers. Insert even or odd.\n", max);
-    even_or_odd_c(even_or_odd, buffer, err03);
-    math(even_or_odd, max, sum, i);
-    return 0;
-}
-
-// Error handler for numbers
-int num_c(int *max, char *err01, char *err02, char *buffer){
-    while(scanf("%d", max) != 1 || max < 0){
-        if(max < 0){
-            printf("%s", err02);
-            fgets(buffer, 100, stdin);
-        }
-        else{
-            printf("%s", err01);
-            fgets(buffer, 100, stdin);
-        }
+void file_delete(void){
+    int len = 50;
+    char* file_name = (char*)malloc((len + 1) * sizeof(char));
+    printf("%s", DELETE_MSG);
+    while(scanf("%s", file_name) != 1){
+        printf("%s at %p", ERR_LEN, file_name);
     }
-    return 0;
-}
-// Error handler for type
-int even_or_odd_c(char *even_or_odd, char *buffer, char *err03){
-    while(scanf("%s", even_or_odd) != 1 || (strcmp(even_or_odd, "even") != 0 && strcmp(even_or_odd, "odd") != 0)){
-        if((strcmp(even_or_odd, "even") != 0) && (strcmp(even_or_odd, "odd") != 0)){
-            printf("%s", err03);
-            fgets(buffer,100,stdin);
-        }
+    int rem = remove(file_name);
+    if(rem == 0){
+        printf("%s", SUCCESS_MSG);
     }
-    return 0;
-}
-// Main calculation code
-int math(char *even_or_odd, int max, int sum, int i){
-    // for even
-    if((strcmp(even_or_odd, "even") == 0)){
-        for(i=1;i<=max;i++){
-            if(i%2==0){
-                sum += i;
-                printf("Iteration : %d | Sum : %d | Memory Address : %p\n", i, sum, &i);
-            }
-        }
-    }
-    // for odd
     else{
-        for(i=1;i<=max;i++){
-            if(i%2!=0){
-                sum += i;
-                printf("Iteration : %d | Sum : %d | Memory Address : %p\n", i, sum, &i);
-            }
-        }
+        printf("%s at %p", ERR_LEN, file_name);
     }
-    return 0;
+    free(file_name);
 }
-// End of the for loop excercise
+void dir_create(void) {
+    int len = 50;
+    char* dir_name = (char*)malloc((len + 1) * sizeof(char));
+    printf("%s", DIR_CREATE_MSG);
+    while (scanf("%s", dir_name) != 1) {
+        printf("%s at %p", ERR_LEN, dir_name);
+    }
+    int creating = mkdir(dir_name, S_IRWXU);
+    if(creating == 0){
+        printf("%s", SUCCESS_MSG);
+        return;
+    }
+    else{
+        printf("%s", ERR_LEN);
+        return;
+    }
+    free(dir_name);
+}
+
+void name(void){
+    int len = 50;
+    char* file_name = (char*)malloc((len + 1) * sizeof(char));
+    char* new_file_name = (char*)malloc((len + 1) * sizeof(char));
+    printf("%s", RENAME_MSG);
+    while(scanf("%s", file_name) != 1){
+        printf("%s", ERR_LEN);
+    }
+    while(scanf("%s", new_file_name) != 1){
+        printf("%s", ERR_LEN);
+    }
+    if(rename(file_name,new_file_name) == 0){
+        printf("%s", ERR_LEN);
+    }
+    else{
+        printf("%s", SUCCESS_MSG);
+    }
+    free(file_name);
+    free(new_file_name);
+}
